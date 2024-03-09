bdc70c54fb89ece108af8d8f75b58ec224cb26fb
Krishnaraj27 <gkrishnaraj2002@gmail.com>
2024-03-09 21:37:01 +0530

    Created rules and messages for register request

---
+++ app/Http/Requests/RegisterCompanyRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterCompanyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email'=>'required|unique:users,email|max:60',
+            'password'=>'required|min:5|max:20',
+            'name'=>'required|max:70',
+            'description'=>'required',
+            'address'=>'required|max:250',
+            'website_url'=>'required|max:250',
+            'mobile'=>'required|digits:10|unique:companies,mobile',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.unique' => 'This email ID is already used. Kindly use another or do login with this email',
+            'mobile.digits' => 'Mobile number should be exactly 10 digits only',
+            'mobile.unique'=>'This mobile number is already used. Kindly use another one'
+
+        ];
+    }
+}
