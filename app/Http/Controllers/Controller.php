82b85379aaabdc194e76cb11f6b1b71e930575d5
Krishnaraj27 <gkrishnaraj2002@gmail.com>
2024-03-22 00:14:47 +0530

    Added function to view register pages for student and company

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function loginPage(Request $request){
+        return view('login');
+    }
+
+    public function registerStudentPage(Request $request){
+        return view('register_student');
+    }
+
+    public function registerCompanyPage(Request $request){
+        return view('company/register_company');
+    }
 }
