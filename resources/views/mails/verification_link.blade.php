5145a05388106f7170855f514dc4fcfa787d85c0
Krishnarajsinh Gohil <gkrishnaraj2002@gmail.com>
2024-03-22 21:49:52 +0530

    Created verification mail blade file

---
+++ resources/views/mails/verification_link.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>OTP</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="container my4">
+        
+        <h3>Welcome {{$name}} to Campus Recruitment System</h3>
+        <br><p>Please click on below link to verify your email address</p>
+        <br> <div> <a href="{{$url}}">Verify</a> </div>
+
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
