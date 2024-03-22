553801a1bb68fbab471925fe0b92d2d80cc41709
Krishnarajsinh Gohil <gkrishnaraj2002@gmail.com>
2024-03-22 21:54:32 +0530

    Created sending verification mail job

---
+++ app/Jobs/SendVerificationMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\VerificationLink;
+
+class SendVerificationMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $data;
+    /**
+     * Create a new job instance.
+     * Requires the url generated and the name of user for sending mail
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $mail = Mail::to($this->data['email'])->send(new VerificationLink([
+            'url'=> $this->data['url'],
+            'name'=> $this->data['name']
+        ]));
+    }
+}
