9838df42a96e56c1b9b1d4abfbc92141b2aaa642
Krishnarajsinh Gohil <gkrishnaraj2002@gmail.com>
2024-03-25 20:11:07 +0530

    Added roles and permissions seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\AssignPermissionsToAdminRole;
+use Database\Seeders\AssignPermissionsToCompanyRole;
+use Database\Seeders\AssignPermissionsToStudentRole;
+use Database\Seeders\CreatePermissions;
+use Database\Seeders\CreateRoles;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +23,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            CreateRoles::class,
+            CreatePermissions::class,
+            AssignPermissionsToAdminRole::class,
+            AssignPermissionsToCompanyRole::class,
+            AssignPermissionsToStudentRole::class
+        ]);
     }
 }
