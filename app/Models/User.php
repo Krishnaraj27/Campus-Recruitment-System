bf07874119dffe3699e25d420ff72c731d9e8c29
Krishnarajsinh Gohil <gkrishnaraj2002@gmail.com>
2024-03-31 00:14:49 +0530

    Added HasRoles trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,10 +10,11 @@ use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use App\Models\Student;
 use App\Models\Company;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     protected $table = 'users';
 
