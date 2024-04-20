fcb12733d3eaf782247ab6b2038fb8f303d57011
Krishnarajsinh Gohil <gkrishnaraj2002@gmail.com>
2024-04-20 14:40:14 +0530

    created mail views for new company registration

---
+++ resources/views/mails/new_company_notification.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>New Company Registration</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="container my4">
+        
+        <h3>Hello Admin</h3>
+        <br><p>{{$companyName}} has recently registered with us. Kindly verify their account from the <a href="{{route('adminDashboard')}}">dashboard</a></p>
+
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ resources/views/mails/welcome_notification_for_company.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome To CRS</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="container my4">
+        
+        <h3>Hello {{$name}}</h3>
+        <br><p>Welcome to Campus Recuitment XYZ. We are grateful that you registered an joined with us.</p>
+        <br><p>Your account details is sent to our administrators and they will be verifying them very soon. It may take upto 3 to 4 working days to verify your account. Meanwhile you can contact the administrator if you have any queries. The contact details are provided in the contact us page of our website.</p>
+        <br><br><br>
+
+        <h5>Thanks and Regards,<br>CRS Admin</h5>
+
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
