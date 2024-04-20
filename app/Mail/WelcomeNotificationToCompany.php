c61b4c0a83f3b9bb57cc0269e414aaf20323c238
Krishnarajsinh Gohil <gkrishnaraj2002@gmail.com>
2024-04-20 14:40:38 +0530

    Created mail class for new company registration

---
+++ app/Mail/WelcomeNotificationToCompany.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class WelcomeNotificationToCompany extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $companyName;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($name)
+    {
+        $this->companyName = $name;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Welcome to Campus Recruitment XYZ',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mails.welcome_notification_for_company',
+            with:[
+                'name'=>$this->companyName
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
